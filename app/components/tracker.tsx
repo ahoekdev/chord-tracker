96109e79223c143523996681d722a62dcd80d794
Arjan <arjan@rakoo.com>
2026-06-08 10:37:07 +0200

    migrate from string array to sections

--- app/components/tracker.tsx
+++ app/components/tracker.tsx
@@ -3,7 +3,16 @@ import useKeyboardInput from "~/hooks/useKeyboardInput";
 function Tracker() {
   const { input } = useKeyboardInput();
 
-  return <div>{input.join(" | ")}</div>;
+  return (
+    <div>
+      {input.map((section) => (
+        <div key={section.name}>
+          <h2>New Section</h2>
+          <p>{section.chords.join(" ")}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Tracker;
--- app/hooks/useKeyboardInput.ts
+++ app/hooks/useKeyboardInput.ts
@@ -1,11 +1,48 @@
 import { useEffect, useState } from "react";
 
+interface Section {
+  name: string;
+  chords: string[];
+}
+
+const chords = new Set<string>(["A", "B", "C", "D", "E", "F", "G"]);
+
 function useKeyboardInput() {
-  const [input, setInput] = useState<string[]>([]);
+  const [sections, setSections] = useState<Section[]>([]);
+
+  const addNewSection = (name: string, chords: string[]) =>
+    setSections((prev) => prev.concat({ name, chords }));
+
+  const handleAddChordToLastSection = (chord: string) =>
+    setSections((prev) =>
+      prev.map((s, i) =>
+        i === prev.length - 1 ? { ...s, chords: [...s.chords, chord] } : s,
+      ),
+    );
+
+  const handleNewSection = (chords: string[]) =>
+    addNewSection("New Section", chords);
 
   useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      setInput((prev) => [...prev, event.key]);
+    const handleKeyDown = ({ key, code }: KeyboardEvent) => {
+      if (code === "Space") {
+        handleNewSection([]);
+        return;
+      }
+
+      const chord = getChordFromKey(key);
+
+      if (!chord) {
+        return;
+      }
+
+      const lastSection = sections[sections.length - 1];
+
+      if (!lastSection) {
+        handleNewSection([chord]);
+      } else {
+        handleAddChordToLastSection(chord);
+      }
     };
 
     window.addEventListener("keydown", handleKeyDown);
@@ -13,9 +50,17 @@ function useKeyboardInput() {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, []);
+  }, [sections, handleNewSection, handleAddChordToLastSection]);
 
-  return { input, setInput };
+  return { input: sections, setInput: setSections };
+}
+
+function getChordFromKey(key: string): string | null {
+  if (chords.has(key.toUpperCase())) {
+    return key.toUpperCase();
+  }
+
+  return null;
 }
 
 export default useKeyboardInput;
