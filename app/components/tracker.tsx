fbd2d960581d9c81a712145b7bafe8daa7e1339c
Arjan <arjan@rakoo.com>
2026-06-08 11:00:37 +0200

    update styling

--- app/components/tracker.tsx
+++ app/components/tracker.tsx
@@ -7,8 +7,15 @@ function Tracker() {
     <div>
       {sections.map(({ name, chords }) => (
         <div key={name}>
-          <h2>{name}</h2>
-          <p>{chords.join(" ")}</p>
+          <h2 className="text-sm font-light text-gray-300">{name}</h2>
+          <div className="flex flex-wrap gap-x-1">
+            {chords.map((chord) => (
+              <span
+                className="bg-gray-100 pl-2 pr-8 py-1"
+                key={chord}
+              >{`${chord}`}</span>
+            ))}
+          </div>
         </div>
       ))}
     </div>
